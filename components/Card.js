fa1bdfa19d9dac2221cc15344be7480653dc4322
Sherpa <81967115+tseten14@users.noreply.github.com>
2022-07-11 00:33:05 -0400

    Project FInalization Project has been finalized

--- components/Card.js
+++ components/Card.js
@@ -1,38 +1,29 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
     /*This is the full component */
     <div className="FullCard">
       {/* This is the Image Component */}
 
       <div className="CardLeft">
-        <img
-          className="photo"
-          src="https://source.unsplash.com/WLxQvbMyfas"
-          alt="mountain"
-        />
+        <img className="photo" src={props.imageUrl} alt="" />
       </div>
 
       {/* This is the Card Component */}
       <div className="CardRight">
-        <small>
-          {" "}
-          Japan &nbsp;
-          <a href="https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!5m1!1e4">
-            View on Google Maps
-          </a>
+        <small className="country">
+          {props.location} &nbsp;
+          <a href={props.googleMapsUrl}>View on Google Maps</a>
         </small>
-        <h1>Mount Fugi</h1>
+        <h1 className="place">{props.title} </h1>
         <p>
           {" "}
-          <strong>Dec 10th 2021- Dec 20 2021 </strong>
+          <strong className="date">
+            {props.startDate} - {props.endDate}{" "}
+          </strong>
         </p>
-        <p>
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <p className="Description">{props.description}</p>
       </div>
     </div>
   );
--- data.js
+++ data.js
@@ -7,26 +7,31 @@ export default [
     endDate: "24 Jan, 2021",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    imageUrl:
+      "https://lh3.googleusercontent.com/7qGDUPijGP5_WDcfMoD6jnbjzutgQWyXfNV_4VPIbj12RJd78LnLeflJ6Wywm_Pk"
   },
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Kathmandu",
+    location: "Nepal",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Kathmandu+44600,+Nepal/@27.7089603,85.3261328,13z/data=!3m1!4b1!4m5!3m4!1s0x39eb198a307baabf:0xb5137c1bf18db1ea!8m2!3d27.7172453!4d85.3239605",
+    startDate: "10 Jan, 2020",
+    endDate: "24 Feb, 2020",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+      "Kathmandu, Nepal's capital, is set in a valley surrounded by the Himalayan mountains. At the heart of the old city’s mazelike alleys is Durbar Square, which becomes frenetic during Indra Jatra, a religious festival featuring masked dances. Many of the city's historic sites were damaged or destroyed by a 2015 earthquake. Durbar Square's palace, Hanuman Dhoka, and Kasthamandap, a wooden Hindu temple, are being rebuilt.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Evening_view_of_the_mountain_range_from_Patan%2C_Lalitpur.jpg/270px-Evening_view_of_the_mountain_range_from_Patan%2C_Lalitpur.jpg"
   },
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Singapore",
+    location: "Singapore",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Singapore/data=!4m2!3m1!1s0x31da11238a8b9375:0x887869cf52abf5c4?sa=X&ved=2ahUKEwjvlryvxOP4AhU8EGIAHf2yAZUQ8gF6BAhtEAE",
+    startDate: "9 Dec, 2020",
+    endDate: "2 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+      "Singapore—known variously as the “Lion City” or “Garden City,” the latter for its many parks and tree-lined streets—has also been called “instant Asia” because it offers the tourist an expeditious glimpse into the cultures brought to it by immigrants from all parts of Asia.",
+    imageUrl:
+      "https://i.natgeofe.com/k/95d61645-a0c7-470f-b198-74a399dd5dfb/singapore-city_square.jpg"
   }
 ];
